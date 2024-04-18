b34f0a5bd71e13b71b25ca7c44d3cbca45320bab
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-04-18 15:32:40 +0200

    test

--- Simulation.py
+++ Simulation.py
@@ -90,24 +90,20 @@ def worker(*args):
   server_mailbox = Mailbox.by_name("Server")
   this_actor.info("server mail box done")
   done = False
-  has_not_asked_for_task = False
   while not done:
     try:
-        if has_not_asked_for_task:
-          #if server_mailbox.ready:
-          this_actor.info("I'm trying to send a request for a task'")
-          has_not_asked_for_task = False
-          comm = server_mailbox.put(Request_For_Task(mailbox), 50)
-          this_actor.info("asked for task")
+      #if server_mailbox.ready:
+      this_actor.info("I'm trying to send a request for a task'")
+      comm = server_mailbox.put(Request_For_Task(mailbox), 50)
+      this_actor.info("asked for task")
         
-        if mailbox.ready:
-          task = mailbox.get()
-          if task.computing_cost > 0: # If compute_cost is valid, execute a computation of that cost 
-            this_actor.info("running:" + str(task.tasknr))
-            has_not_asked_for_task = True
-            this_actor.execute(task.computing_cost)
-            #add task done code here
-          else: # Stop when receiving an invalid compute_cost
+      if mailbox.ready:
+        task = mailbox.get()
+        if task.computing_cost > 0: # If compute_cost is valid, execute a computation of that cost 
+          this_actor.info("running:" + str(task.tasknr))
+          this_actor.execute(task.computing_cost)
+          #add task done code here
+        else: # Stop when receiving an invalid compute_cost
             done = True
             this_actor.info("Exiting now.")
     except Exception as e:
