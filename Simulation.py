02f6f4612351d4858189d24f6417e1a046d6823e
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-04-21 21:18:33 +0200

    fix

--- Simulation.py
+++ Simulation.py
@@ -103,7 +103,7 @@ def master(*args):
         comm = worker_mailbox.put_init(task, task.communication_cost)
         comm.wait_for(5)
 
-      elif len(tasks) == 0 and len(sent_tasks) > 0:
+      elif len(tasks) == 0 and len(sent_tasks) > 1:
         worker_mailbox = Mailbox.by_name(str(data.mailbox)[8:-1])
         this_actor.info(str(data))
         this_actor.info("sending wait to:" + str(data.mailbox)[8:-1])
