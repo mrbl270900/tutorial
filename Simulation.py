477f355aa6ea1121be5009390ff650c77ffd7a01
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-04-21 10:41:45 +0200

    test

--- Simulation.py
+++ Simulation.py
@@ -63,7 +63,7 @@ def master(*args):
         this_actor.info("mailbox ready")
         data = server_mailbox.get()
         this_actor.info(str(data))
-        worker_mailbox = Mailbox.by_name(str(data.mailbox))
+        worker_mailbox = Mailbox.by_name(str(data.mailbox)[8:-1])
         this_actor.info("sending " + str(tasks[0].tasknr) + " to:" + str(data.mailbox))
         task = tasks[0]
         tasks.remove(tasks[0])
