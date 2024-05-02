1e7bcd099ac29e333e54a106a112ecb66b3f70e2
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-05-02 23:30:06 +0200

    test

--- Simulation.py
+++ Simulation.py
@@ -87,7 +87,8 @@ def master(*args):
 
       while True:
         comm_get = server_mailbox.get_async()
-        if type(comm_get.sender.name) == None:
+        this_actor.info(str(comm_get.sender))
+        if comm_get.sender == None:
           this_actor.info(str(comm_get.sender))
           break
         else:
