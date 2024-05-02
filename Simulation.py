7ea4fbcfa2360bb7d9b553eefefe26186c2feb80
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-05-02 13:29:07 +0200

    fix for worker timeout

--- Simulation.py
+++ Simulation.py
@@ -160,7 +160,7 @@ def worker(*args):
       if not_asked_for_task:
         this_actor.info("I'm trying to send a request for a task")
         comm = server_mailbox.put_init(Request_For_Task(str(mailbox)), 50)
-        comm.wait_for(5)
+        comm.wait()
         not_asked_for_task = False
         
       else:
