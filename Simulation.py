a8384388b3c5bad3fe96aee5e9b2e6abbc922e7a
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-05-25 12:18:14 +0200

    random

--- Simulation.py
+++ Simulation.py
@@ -356,7 +356,7 @@ def master(*args):
   last_run_sent_tasks_check = Time.get_time()
   sending_comms = []
   not_done = True
-  alg = "score"
+  alg = "random"
   chunck = 1
   low_low = []
   med_low = []
