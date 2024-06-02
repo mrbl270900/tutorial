059983acce0fd2e0cda7a3dc84abb990146ff9a9
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-06-02 16:48:23 +0200

    big first 1 task

--- Simulation.py
+++ Simulation.py
@@ -358,7 +358,7 @@ def master(*args):
   last_run_sent_tasks_check = Time.get_time()
   sending_comms = []
   not_done = True
-  alg = "small first"
+  alg = "big first"
   chunck = 1
   low_low = []
   med_low = []
