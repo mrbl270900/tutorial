eb936d4de69b0c644523ac8c9f87fa1f451f3592
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-06-02 18:46:05 +0200

    small first 1 task at a time

--- Simulation.py
+++ Simulation.py
@@ -358,8 +358,8 @@ def master(*args):
   last_run_sent_tasks_check = Time.get_time()
   sending_comms = []
   not_done = True
-  alg = "catagory"
-  chunck = 3
+  alg = "small first"
+  chunck = 1
   low_low = []
   med_low = []
   high_low = []
