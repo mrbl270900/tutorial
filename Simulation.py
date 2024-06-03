34dabf579e58a76bb0c8b75990942ea0ac6d1414
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2024-06-03 18:22:35 +0200

    big first 10 tasks at a time

--- Simulation.py
+++ Simulation.py
@@ -358,7 +358,7 @@ def master(*args):
   last_run_sent_tasks_check = Time.get_time()
   sending_comms = []
   not_done = True
-  alg = "score"
+  alg = "big first"
   chunck = 10
   low_low = []
   med_low = []
